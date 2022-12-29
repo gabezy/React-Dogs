e60d95cccfb23f982e7af31f66f513fb8c35ec7b
gabezy <gabrielmoreira2705@gmail.com>
2022-12-29 10:16:09 -0300

    header

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
       <BrowserRouter>
         <Header />
         <Routes>
-          <Route path="" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
         </Routes>
         <Footer />
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,13 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styles from "./Header.module.css";
+import { ReactComponent as Dogs } from "../Assets/dogs.svg";
 
 const Header = () => {
   return (
     <header className={styles.header}>
-      <nav>
-        <Link to="">Home</Link>
-        <Link to="/login">Login / Criar</Link>
+      <nav className={`${styles.nav} container`}>
+        <Link to="/" className={styles.logo} aria-label="Dogs - Home">
+          <Dogs />
+        </Link>
+        <Link to="/login" className={styles.login}>
+          Login / Criar
+        </Link>
       </nav>
     </header>
   );
