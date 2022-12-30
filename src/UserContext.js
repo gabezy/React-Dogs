6104f7e017f9bb283ecf4c10d9010ddbe1b5d1cf
gabezy <gabrielmoreira2705@gmail.com>
2022-12-30 20:41:55 -0300

    Protected Route

--- src/UserContext.js
+++ src/UserContext.js
@@ -82,6 +82,8 @@ export const UserStorage = ({ children }) => {
         } finally {
           setLoading(false);
         }
+      } else {
+        setLogin(false);
       }
     }
     autoLogin();
